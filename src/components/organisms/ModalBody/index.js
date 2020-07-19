57b8d1a4888813f7449d622f7dd830be7886e897
rifanid98 <adninsijawa@gmail.com>
2020-07-20 01:22:54 +0700

    new components: ListItems, ModalBody, ScreenGridItems

---
+++ src/components/organisms/ModalBody/index.js
@@ -0,0 +1,11 @@
+import BookModalBody from './BookModalBody'
+import GenreModalBody from './GenreModalBody'
+import AuthorModalBody from './AuthorModalBody'
+import UserModalBody from './UserModalBody'
+
+export {
+  BookModalBody,
+  GenreModalBody,
+  AuthorModalBody,
+  UserModalBody
+}
