175843e41305489345a9c41abdc03318c4508e25
rifanid98 <adninsijawa@gmail.com>
2020-07-20 18:09:17 +0700

    remove comment and some import codes

--- src/gates/AuthGate.js
+++ src/gates/AuthGate.js
@@ -1,18 +1,10 @@
 import React, { useEffect } from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import { Login, Signup2, Signup1 } from 'screens'
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
-import { button } from 'assets';
-import { View, Text } from 'react-native';
 
 const AuthGate = (props) => {
   const Stack = createStackNavigator();
-  const {
-    getStarted
-  } = props;
   
-
   return (
     <>
       <Stack.Navigator initialRouteName='login' screenOptions={{
@@ -24,16 +16,6 @@ const AuthGate = (props) => {
           component={Login} />
         <Stack.Screen name="signup1" component={Signup1} options={{title: 'Sign Up Screen'}} />
         <Stack.Screen name="signup2" component={Signup2} options={{title: 'Sign Up Screen'}} />
-        {/* <Stack.Screen
-          name="signup"
-          component={Signup}
-          options={{
-            headerShown: true,
-            headerTitle: null,
-            headerLeft: () => (
-              <View style={button.headerBackButton}><FontAwesomeIcon icon={faArrowLeft} size="20" /></View>
-            )
-          }} /> */}
       </Stack.Navigator>
     </>
   )
