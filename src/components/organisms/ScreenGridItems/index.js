c8457e1652870cc1a83d8a4ac48a47d6af440548
rifanid98 <adninsijawa@gmail.com>
2020-07-21 19:17:44 +0700

    add book image and description

--- src/components/organisms/ScreenGridItems/index.js
+++ src/components/organisms/ScreenGridItems/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler';
 import { organismsStyles as styles} from 'assets/styles';
 import { SearchGridItem } from 'components/molecules';
 
 const ScreenGridItems = (props) => {
-  const books = props.booksData
+  
+  useEffect(() => {
+    
+  }, [])
+  const books = props.booksData.length ? props.booksData : []
   return (
     <>
       <ScrollView>
@@ -14,11 +18,13 @@ const ScreenGridItems = (props) => {
             <SearchGridItem
               key={index}
               bookId={book.book_id}
+              image={book.image}
               title={book.title}
               author_name={book.author_name}
               genre_name={book.genre_name}
+              description={book.description}
               navigation={props.navigation}
-              />
+            />
           ))}
         </View>
       </ScrollView>
