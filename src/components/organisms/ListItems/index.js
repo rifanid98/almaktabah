a9c9ccbc7af98f254f7ff65658c8ed3f9f1463a5
rifanid98 <adninsijawa@gmail.com>
2020-07-21 19:17:09 +0700

    add not found feedback

--- src/components/organisms/ListItems/index.js
+++ src/components/organisms/ListItems/index.js
@@ -3,6 +3,7 @@ import { View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler';
 import { organismsStyles as styles} from 'assets/styles';
 import { ListItem } from 'components/molecules';
+import { NotFound } from 'components/atoms';
 
 const ListItems = (props) => {
   const datas = props.data
@@ -10,14 +11,20 @@ const ListItems = (props) => {
     <>
       <ScrollView>
         <View style={styles.listItems}>
-          {datas.map((data, index) => (
-            <ListItem
-              key={index}
-              image={data.image}
-              info={data.info}
-              layout={props.layout}
-              />
-          ))}
+          {
+            datas
+              ? datas.length > 0
+                ? datas.map((data, index) => (
+                  <ListItem
+                    key={index}
+                    image={data.image}
+                    info={data.info}
+                    layout={props.layout}
+                  />
+                ))
+                : <NotFound />
+              : <NotFound />
+          }
         </View>
       </ScrollView>
     </>
