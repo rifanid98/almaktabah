cd3557a8d49829b69dbe7a79a1786a0e14d207f6
rifanid98 <adninsijawa@gmail.com>
2020-07-22 21:20:58 +0700

    search screen is working now

--- src/screens/Search/SearchAllBooks/index.js
+++ src/screens/Search/SearchAllBooks/index.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View } from 'react-native'
-import { ScreenHeader, ScreenSearch, ScreenFilter, ScreenGridItems } from 'components'
+import { ScreenHeader, ScreenSearch, ScreenFilter, ScreenGridItems, Errors, Loading, Search, Empty } from 'components'
 import { searchStyles as styles } from 'assets/styles'
+import { connect } from 'react-redux'
+import { getBooks } from 'modules'
+import { createUrlParamFromObj } from 'utils'
 
 const SearchAllBooks = (props) => {
+  const [search, setSearch] = useState(false)
   const filterItems = {
     sortType: {
       asc: 'A-Z',
@@ -14,42 +18,68 @@ const SearchAllBooks = (props) => {
       date: 'newest'
     }
   }
-  const booksData = [
-    {
-      book_id: 1,
-      title: 'Book 1',
-      author_name: 'Author 1',
-      genre_name: 'Genre 1'
-    },
-    {
-      book_id: 2,
-      title: 'Book 2',
-      author_name: 'Author 2',
-      genre_name: 'Genre 2'
-    },
-    {
-      book_id: 3,
-      title: 'Book 3',
-      author_name: 'Author 3',
-      genre_name: 'Genre 3'
-    },
-    {
-      book_id: 4,
-      title: 'Book 4',
-      author_name: 'Author 4',
-      genre_name: 'Genre 4'
-    },
-  ]
+  useEffect(() => {
+    console.log(props.books, 'props books');
+  }, [props.books])
+
+  const getBooksByKeyword = (keyword) => {
+    setSearch(true)
+    const search = {
+      author: keyword,
+      genre: keyword,
+      description: keyword,
+      title: keyword,
+      added: keyword,
+      page: 1,
+      limit: 10
+    }
+    const params = createUrlParamFromObj(search);
+    console.log(params);
+    const token = props.auth.data.tokenLogin
+    if (token) {
+      props.getBooks(token, params)
+        .then((res) => {
+          
+        }).catch((error) => {
+          console.log(`get books by keyword failed`)
+        })
+    }
+  }
   return (
     <>
       <View style={styles.container}>
         <ScreenHeader navigation={props.navigation} title="Search..." />
-        <ScreenSearch navigation={props.navigation} route={props.route} />
+        <ScreenSearch navigation={props.navigation} route={props.route} onPress={(keyword) => getBooksByKeyword(keyword)} />
         <ScreenFilter filterItems={filterItems} />
-        <ScreenGridItems navigation={props.navigation} booksData={booksData} />
+        {
+          search
+            ? props.books.isLoading
+              ? <Loading />
+              : props.books.data.result
+                ? props.books.data.result.length > 0
+                  ? <ScreenGridItems navigation={props.navigation} booksData={props.books.data.result} />
+                  : <Empty />
+
+              : props.books.data
+                  ? props.books.data.length > 0
+                    ? <ScreenGridItems navigation={props.navigation} booksData={props.books.data} />
+                    : <Empty />
+                  : <Erros />
+                
+            : <Search />
+        }
       </View>
     </>
   )
 }
 
-export default SearchAllBooks;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  books: state.books
+})
+
+const mapDispatchToProps = {
+  getBooks
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchAllBooks)
