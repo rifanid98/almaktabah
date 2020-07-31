0fc7274fb9eb2d2bb5a99879ef04d6c8d3c63e99
rifanid98 <adninsijawa.office@gmail.com>
2020-07-31 21:04:50 +0700

    change aler code

--- src/components/organisms/ModalBody/AddModalBody/BookAddModalBody/index.js
+++ src/components/organisms/ModalBody/AddModalBody/BookAddModalBody/index.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, TextInput, ImageBackground, TouchableOpacity, ScrollView, Image, Alert } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, ScrollView, Image, Alert } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
 import Textarea from 'react-native-textarea';
 import { Picker } from '@react-native-community/picker';
 import { managerStyles as styles, colorScheme as color} from "assets/styles";
-import { getAuthors, getGenres, patchBook, getBooks, addBook } from 'modules';
+import { getAuthors, getGenres, getBooks, addBook } from 'modules';
 import { connect } from 'react-redux';
-import { alert, createUrlParamFromObj, createImageFormData } from 'utils';
+import { createUrlParamFromObj, createImageFormData } from 'utils';
 import ImagePicker from 'react-native-image-picker'
 
 const BookAddModalBody = (props) => {
@@ -47,7 +47,7 @@ const BookAddModalBody = (props) => {
         }).catch((error) => {
           console.log(`get authors failed`)
         })
-      : alert('Token Failed', 'Cannot find token...')
+      : Alert.alert('Token Failed', 'Cannot find token...')
   }
   const getGenres = () => {
     const token = props.auth.data.tokenLogin;
@@ -58,7 +58,7 @@ const BookAddModalBody = (props) => {
         }).catch((error) => {
           console.log(`get genres failed`)
         })
-      : alert('Token Failed', 'Cannot find token...')
+      : Alert.alert('Token Failed', 'Cannot find token...')
   }
   const getBooks = () => {
     const pagination = {
@@ -74,7 +74,7 @@ const BookAddModalBody = (props) => {
         }).catch((error) => {
           console.log(`get books failed`)
         })
-      : alert('Token Failed', 'Cannot find token...')
+      : Alert.alert('Token Failed', 'Cannot find token...')
   }
   const addBook = (data) => {
     const token = props.auth.data.tokenLogin;
@@ -89,8 +89,8 @@ const BookAddModalBody = (props) => {
       .catch((error) => {
         console.log(error);
         error.response.data.message
-          ? alert('Failed', error.response.data.message)
-          : alert('Failed', 'Add Book Failed. Pleas try again later')
+          ? Alert.alert('Failed', error.response.data.message)
+          : Alert.alert('Failed', 'Add Book Failed. Pleas try again later')
       })
   }
   
